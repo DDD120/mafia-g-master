297254893e2e3060902ae30e7ffc8fa267857f2a
DDD120 <dltnals9794@naver.com>
2023-10-17 14:02:29 +0900

    feat UI 애니메이션 적용

--- components/BasicModal.tsx
+++ components/BasicModal.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react"
 import { createPortal } from "react-dom"
 import Button from "./button/Button"
+import { useSpring, animated, easings } from "@react-spring/web"
 
 interface Props {
   title: string
@@ -12,11 +13,22 @@ interface Props {
 
 function BasicModal({ children, title, onClose }: Props) {
   const [render, setRender] = useState<HTMLDivElement | null>(null)
+  const spring = useSpring({
+    from: { opacity: 0, y: 20, transform: `translate(-50%, -50%)` },
+    to: { opacity: 1, y: 0, transform: `translate(-50%, -50%)` },
+    config: {
+      easing: easings.easeOutQuint,
+      duration: 300,
+    },
+  })
 
   const renderModal = () => {
     if (!render) return
     return createPortal(
-      <div className="max-h-[90%] sm:max-w-[600px] h-auto bg-black border-2 border-opacity-50 border-red fixed p-4 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-11/12 rounded-lg overflow-hidden flex">
+      <animated.div
+        style={spring}
+        className="max-h-[90%] sm:max-w-[600px] h-auto bg-black border-2 border-opacity-50 border-red fixed p-4 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-11/12 rounded-lg overflow-hidden flex"
+      >
         <div className="flex-1 flex flex-col items-center gap-4">
           <h2 className="font-bold text-2xl">{title}</h2>
           <div className="scrollbar-hide overflow-y-auto w-full">
@@ -31,7 +43,7 @@ function BasicModal({ children, title, onClose }: Props) {
             확인
           </Button>
         </div>
-      </div>,
+      </animated.div>,
       document.getElementById("modal")!
     )
   }
--- components/results/Congratulation.tsx
+++ components/results/Congratulation.tsx
@@ -1,17 +1,30 @@
 import { winnerToKorean } from "@/lib/toKorean"
 import FirecrackerLottie from "./FirecrackerLottie"
+import { useSpring, animated, easings } from "@react-spring/web"
 
 interface Props {
   winner: "mafia" | "citizen" | null
 }
 
 function Congratulation({ winner }: Props) {
+  const spring = useSpring({
+    from: { opacity: 0, fontSize: "24px" },
+    to: { opacity: 1, fontSize: "36px" },
+    config: {
+      easing: easings.easeOutBack,
+      duration: 500,
+    },
+  })
+
   return (
     <div className="relative">
-      <h1 className="mt-16 mb-20 text-center text-4xl font-black">
+      <animated.h1
+        style={spring}
+        className="mt-16 mb-20 text-center text-4xl font-black"
+      >
         {winner && winnerToKorean[winner]}이 <br />
         승리하였습니다!
-      </h1>
+      </animated.h1>
       <div className="w-full absolute left-1/2 top-[-70px] -translate-x-1/2">
         <FirecrackerLottie />
       </div>
